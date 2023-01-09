358ffae4c079e7bce34f394b9a96437c588ee7b7
BroccoliSurprise <61516592+BroccoliSurprise@users.noreply.github.com>
2023-01-09 20:59:35 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        bitbot.ledShift()
-    }
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    bitbot.ledRotate()
-    basic.pause(100)
-    bitbot.ledClear()
-    bitbot.setPixelColor(0, bitbot.convertRGB(0, 0, 0))
-})
-bitbot.setLedColor(0xFF0000)
 basic.forever(function () {
-    bitbot.ledRainbow()
+	
 })
